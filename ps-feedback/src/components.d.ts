1e30cdd0b2b92e94ee310162843710fdeb9f524d
Projckt <projckt@protonmail.com>
2022-02-24 09:22:05 +0530

    adds eventEmitter

--- ps-feedback/src/components.d.ts
+++ ps-feedback/src/components.d.ts
@@ -6,56 +6,41 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
+    interface PsFeedback {
+        "audioUpload": boolean;
+        "imageUpload": boolean;
+        "inputLabel": string;
+        "options": any;
     }
 }
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
+    interface HTMLPsFeedbackElement extends Components.PsFeedback, HTMLStencilElement {
     }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
+    var HTMLPsFeedbackElement: {
+        prototype: HTMLPsFeedbackElement;
+        new (): HTMLPsFeedbackElement;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
+        "ps-feedback": HTMLPsFeedbackElement;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
+    interface PsFeedback {
+        "audioUpload"?: boolean;
+        "imageUpload"?: boolean;
+        "inputLabel"?: string;
+        "onPsFeedbackEvent"?: (event: CustomEvent<any>) => void;
+        "options"?: any;
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
+        "ps-feedback": PsFeedback;
     }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "ps-feedback": LocalJSX.PsFeedback & JSXBase.HTMLAttributes<HTMLPsFeedbackElement>;
         }
     }
 }
--- ps-feedback/src/components/ps-feedback/ps-feedback.tsx
+++ ps-feedback/src/components/ps-feedback/ps-feedback.tsx
@@ -1,14 +1,53 @@
-import { Component, h } from '@stencil/core';
+import { Component, State, Prop, Event, EventEmitter, h } from '@stencil/core';
 
 @Component({
   tag: 'ps-feedback',
   styleUrl: 'ps-feedback.css',
 })
 export class PsFeedback {
+  @State() compState: string = 'init';
+
+  @Prop() options: any = [
+    { label: 'Choose option', value: '' },
+    { label: 'Bug', value: 'bug' },
+    { label: 'Idea', value: 'idea' },
+    { label: 'Others', value: 'others' },
+  ];
+  @Prop() inputLabel: string = 'default';
+  @Prop() imageUpload: boolean = false;
+  @Prop() audioUpload: boolean = false;
+
+  @Event({
+    eventName: 'psFeedbackEvent',
+    bubbles: true,
+  })
+  psFeedbackEventEmitter: EventEmitter;
+
+  eventDispatcher(name: string, data: any) {
+    this.psFeedbackEventEmitter.emit({
+      name: name,
+      data: data,
+    });
+  }
+
   render() {
     return (
-      <div>
-        <p>Hello PsFeedback!</p>
+      <div id="ps-feedback-container">
+        <input id="ps-feedback-name" type="text" placeholder="name" />
+        <input id="ps-feedback-email" type="email" placeholder="email" />
+        <select id="ps-feedback-select">
+          {this.options.map(option => (
+            <option value={option.value}>{option.label}</option>
+          ))}
+        </select>
+        <textarea id="ps-feedback-textarea"></textarea>
+        <ul id="ps-feedback-upload-list">
+          <li class="ps-feedback-upload-list-item">image.jpg</li>
+          <li class="ps-feedback-upload-list-item">audio.png</li>
+        </ul>
+        {this.imageUpload && <button id="ps-feedback-image-upload-btn">Upload Image</button>}
+        {this.audioUpload && <button id="ps-feedback-audio-upload-btn">Upload Audio</button>}
+        <button id="ps-feedback-submit-btn">Submit</button>
       </div>
     );
   }
