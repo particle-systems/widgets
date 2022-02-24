312217fc94e824d3896f8e7069d1e874947368dc
Projckt <projckt@protonmail.com>
2022-02-24 07:39:32 +0530

    adds component

---
+++ ps-feedback/src/components/ps-feedback/ps-feedback.tsx
@@ -0,0 +1,15 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'ps-feedback',
+  styleUrl: 'ps-feedback.css',
+})
+export class PsFeedback {
+  render() {
+    return (
+      <div>
+        <p>Hello PsFeedback!</p>
+      </div>
+    );
+  }
+}
