03155e1a8617aca9526dde52dc48c6f62e87d6b9
Projckt <projckt@protonmail.com>
2022-02-26 12:03:07 +0530

    adds more structure to code

--- ps-feedback/src/components/ps-feedback/ps-feedback.tsx
+++ ps-feedback/src/components/ps-feedback/ps-feedback.tsx
@@ -1,4 +1,17 @@
-import { Component, State, Prop, Event, EventEmitter, h } from '@stencil/core';
+import { Component, State, Prop, Event, EventEmitter, FunctionalComponent, h } from '@stencil/core';
+
+interface TextInputProps {
+  type: string;
+  name: string;
+  id: string;
+  placeholder: string;
+}
+
+interface ButtonProps {
+  name: string;
+  id: string;
+  label: string;
+}
 
 @Component({
   tag: 'ps-feedback',
@@ -8,14 +21,24 @@ export class PsFeedback {
   @State() compState: string = 'init';
 
   @Prop() options: any = [
-    { label: 'Choose option', value: '' },
-    { label: 'Bug', value: 'bug' },
-    { label: 'Idea', value: 'idea' },
-    { label: 'Others', value: 'others' },
+    { label: 'Choose option', value: '', iconUrl: '' },
+    { label: 'Bug', value: 'bug', iconUrl: '' },
+    { label: 'Idea', value: 'idea', iconUrl: '' },
+    { label: 'Others', value: 'others', iconUrl: '' },
   ];
   @Prop() inputLabel: string = 'default';
   @Prop() imageUpload: boolean = false;
   @Prop() audioUpload: boolean = false;
+
+  private name: string;
+  private email: string;
+  private message: string;
+  private uploads: any;
+  private feedbackOptions: any = [
+    { label: 'Idea', value: 'idea' },
+    { label: 'Bug', value: 'bug' },
+    { label: 'Others', value: 'other' },
+  ];
 
   @Event({
     eventName: 'psFeedbackEvent',
@@ -30,24 +53,70 @@ export class PsFeedback {
     });
   }
 
+  handleTextInput(e, name: string) {
+    let sanitisedInput = e.target.value.trim();
+    if (!sanitisedInput) return;
+
+    if (name === 'name') {
+      this.name = sanitisedInput;
+    } else if (name === 'email') {
+      this.email = sanitisedInput;
+    }
+  }
+
+  handleFeedbackChoice(e) {}
+
+  handleButtonClick(name: string) {
+    console.log(`buttonName: ${name}`);
+  }
+
   render() {
+    const TextInput: FunctionalComponent<TextInputProps> = ({ type, name, id, placeholder }) => (
+      <input type={type} id={id} name={name} placeholder={placeholder} onInput={e => this.handleTextInput(e, name)}></input>
+    );
+
+    const FeedbackOptions: FunctionalComponent = () => (
+      <div class="feedback-option-group">
+        {this.feedbackOptions.map(option => [
+          <input
+            class="ps-feedback-option-input"
+            id={option.value}
+            type="radio"
+            name="feedbackOption"
+            value={option.value}
+            onChange={e => this.handleFeedbackChoice(e)}
+            disabled={this.compState === 'submitting' ? true : false}
+          ></input>,
+          <label class="ps-feedback-option-label" htmlFor={option.value}>
+            {option.label}
+          </label>,
+        ])}
+      </div>
+    );
+
+    const Button: FunctionalComponent<ButtonProps> = ({ name, id, label }) => (
+      <button id={id} class="control-btn" onClick={() => this.handleButtonClick(name)}>
+        {label}
+      </button>
+    );
+
     return (
       <div id="ps-feedback-container">
-        <input id="ps-feedback-name" type="text" placeholder="name" />
-        <input id="ps-feedback-email" type="email" placeholder="email" />
-        <select id="ps-feedback-select">
-          {this.options.map(option => (
-            <option value={option.value}>{option.label}</option>
-          ))}
-        </select>
+        <TextInput type="text" name="name" id="ps-feedback-name" placeholder="Name"></TextInput>
+        <TextInput type="email" name="email" id="ps-feedback-email" placeholder="Email"></TextInput>
+        <FeedbackOptions></FeedbackOptions>
         <textarea id="ps-feedback-textarea"></textarea>
         <ul id="ps-feedback-upload-list">
           <li class="ps-feedback-upload-list-item">image.jpg</li>
           <li class="ps-feedback-upload-list-item">audio.png</li>
         </ul>
-        {this.imageUpload && <button id="ps-feedback-image-upload-btn">Upload Image</button>}
-        {this.audioUpload && <button id="ps-feedback-audio-upload-btn">Upload Audio</button>}
-        <button id="ps-feedback-submit-btn">Submit</button>
+        <div class="ps-feedback-btn-group">
+          <div class="ps-feedback-upload-btn-group">
+            {this.imageUpload && <Button name="imageUpload" id="ps-feedback-upload-image-btn" label="Upload Image"></Button>}
+            {this.imageUpload && <Button name="recordAudio" id="ps-feedback-record-audio-btn" label="Record Audio"></Button>}
+          </div>
+          <Button name="submitFeedback" id="ps-feedback-submit-btn" label="Submit"></Button>
+        </div>
       </div>
     );
   }
